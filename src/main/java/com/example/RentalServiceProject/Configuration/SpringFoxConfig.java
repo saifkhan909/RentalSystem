b6f481700b585195621004351d728745a5a148cd
shahzadiqbal <shahzadiqbal.may18@gmail.com>
2022-06-25 20:00:28 +0500

    swagger changes

--- src/main/java/com/example/RentalServiceProject/Configuration/SpringFoxConfig.java
+++ src/main/java/com/example/RentalServiceProject/Configuration/SpringFoxConfig.java
@@ -1,20 +1,35 @@
 package com.example.RentalServiceProject.Configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableWebMvc
 @Configuration
-@EnableSwagger2WebMvc
-public class SpringFoxConfig {
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("swagger-ui.html")
-//                .addResourceLocations("classpath:/META-INF/resources/");
-//
-//        registry.addResourceHandler("/webjars/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-//    }
+@EnableSwagger2
+public class SpringFoxConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.any())
 
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //enabling swagger-ui part for visual documentation
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 
 
 }
